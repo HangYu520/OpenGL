5d9ce256cfd49834732e2b448e8b372233fb96d9
HangYu520 <hyu02246@gmail.com>
2025-12-11 15:44:26 +0800

    Update maincallback.h

--- include/maincallback.h
+++ include/maincallback.h
@@ -105,7 +105,7 @@ inline void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 * 键盘输入处理
 * ---------------------
 */
-inline void processInput(GLFWwindow* window) 
+static void processInput(GLFWwindow* window) 
 {
     if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) // 检测 ESC 键, 退出程序
         glfwSetWindowShouldClose(window, true);
