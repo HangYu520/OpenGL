12132ac606d284df756fd38838f30cef8b566601
HangYu520 <hyu02246@gmail.com>
2025-12-18 19:23:22 +0800

    Create SingleColorShader.fs

---
+++ shader/SingleColorShader.fs
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoord;
+
+uniform sampler2D texture1;
+
+// Main
+void main()
+{
+   FragColor = vec4(0.04, 0.28, 0.26, 1.0);
+}
